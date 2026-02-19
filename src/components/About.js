2d54b3bfa117ab61589f8a6c7f55c3eae95142c1
Zeitun <laila.ahmed.2645@gmail.com>
2026-02-19 23:22:48 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { image } from "../data/data";
-
-function About() {
-  return <div>About</div>;
+function About({ image }) {
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>
+        I am passionate about web development and enjoy building creative and
+        user-friendly applications.
+      </p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
